bda1756cd4fe17af597e708b26a5697b70ed1aa9
ctabordaz <ctabordaz@unal.edu.co>
2015-07-25 17:50:45 -0500

    master layout II

--- app/views/administrador/index.blade.php
+++ app/views/administrador/index.blade.php
@@ -1,11 +1,43 @@
-<html>
-    <head>
-        <title>Administrador</title>
-    </head>
-    <body>
-        <h1>Bienvendo</h1>
-        @foreach($estudiantes as $estudiante)
-    <li>{{$estudiante->nombre}} - <a href="{{URL::to('/administrador/asignar-citacion', array($estudiante->identificacion))}}">Asignar Cita</a></li>
-        @endforeach
-    </body>
-</html>
+@extends('layouts.master')
+
+@section('titulo')
+Asignar Cita
+@stop 
+
+@section('options')
+<li class="treeview">
+    <a href="#">
+        <i class="fa fa-files-o"></i>
+        <span>Layout Options</span>
+        <span class="label label-primary pull-right">4</span>
+    </a>
+    <ul class="treeview-menu">
+        <li><a href="pages/layout/top-nav.html"><i class="fa fa-circle-o"></i> Top Navigation</a></li>
+        <li><a href="pages/layout/boxed.html"><i class="fa fa-circle-o"></i> Boxed</a></li>
+        <li><a href="pages/layout/fixed.html"><i class="fa fa-circle-o"></i> Fixed</a></li>
+        <li><a href="pages/layout/collapsed-sidebar.html"><i class="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
+    </ul>
+</li>
+<li>
+    <a href="pages/widgets.html">
+        <i class="fa fa-th"></i> <span>Widgets</span> <small class="label pull-right bg-green">new</small>
+    </a>
+</li>
+
+
+
+
+<li>
+    <a href="pages/calendar.html">
+        <i class="fa fa-calendar"></i> <span>Calendar</span>
+        <small class="label pull-right bg-red">3</small>
+    </a>
+</li>
+<li>
+    <a href="pages/mailbox/mailbox.html">
+        <i class="fa fa-envelope"></i> <span>Mailbox</span>
+        <small class="label pull-right bg-yellow">12</small>
+    </a>
+</li>
+
+@stop
